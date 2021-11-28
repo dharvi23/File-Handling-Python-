cb81e8d34b89afca8302d170d0c83ba7326e9b59
dharvi23 <77873714+dharvi23@users.noreply.github.com>
2021-11-28 18:38:18 +0530

    Add files via upload

---
+++ File Handling.py
@@ -0,0 +1,34 @@
+import pathlib as p
+import os
+
+Path = ""
+ext = set()
+
+def GetExtintions():
+    for i in files:
+        ext.add(i.suffix.replace(".",""))
+
+def CreateFolder():
+    for i in ext:
+        try:
+            os.mkdir(str(Path / i))
+        except:
+            pass
+
+def MoveFiles():
+    for i in files:
+        if i.is_file():
+            print(Path / i.suffix.replace(".","") / i.name)
+            os.rename(str(i), Path / i.suffix.replace(".","") / i.name)
+            
+def main():
+    global files,Path
+
+    Path = p.Path(input("Enter Path: "))
+    files = list(Path.glob("*"))
+
+    GetExtintions()
+    CreateFolder()
+    MoveFiles()
+
+main()
